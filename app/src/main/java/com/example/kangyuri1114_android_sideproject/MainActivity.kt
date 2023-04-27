15a5cab190cdad0360106ddf1453b4b37cb24c9c
kangyuri1114 <yuri3334@gmail.com>
2023-04-28 00:31:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kangyuri1114_android_sideproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kangyuri1114_android_sideproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
